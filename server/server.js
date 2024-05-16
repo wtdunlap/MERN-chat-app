98542fb5e129001f59fba3876378293c19060641
834R807 <150474122+wtdunlap@users.noreply.github.com>
2024-05-16 16:23:25 -0400

    Adding more and fleshing out some components

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import express from "express";
-import dotenv from "doenv";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./config/connection.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello Whirled")
-})
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => {
+    connectDB();
+    console.log(
+        `API server running on port ${PORT} at http://localhost:${PORT}`
+    );
+});
