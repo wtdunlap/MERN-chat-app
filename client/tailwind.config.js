42dee427ce44d936ff18daf4ec5e6d4ebab402eb
834R807 <150474122+wtdunlap@users.noreply.github.com>
2024-05-29 00:31:46 -0400

    Login page is done for now

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,html,ts,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        daisyui,
+      ],
+};
